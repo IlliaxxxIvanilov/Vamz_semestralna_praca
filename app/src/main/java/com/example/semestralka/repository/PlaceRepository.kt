b324c4ec6d1930b1bb03be75403f41b31c702f9e
IlliaxxxIvanilov <illia.ivanilov04@gmail.com>
2026-05-20 20:08:13 +0200

    +add PlacesRepository interface

---
+++ app/src/main/java/com/example/semestralka/repository/PlaceRepository.kt
@@ -0,0 +1,20 @@
+package com.example.semestralka.repository
+
+import com.example.semestralka.model.Category
+import com.example.semestralka.model.Place
+import kotlinx.coroutines.flow.Flow
+
+interface PlacesRepository {
+
+    fun getPlaces(): Flow<List<Place>>
+
+    fun getPlacesByCategory(category: Category): Flow<List<Place>>
+
+    fun getFavoriteIds(): Flow<Set<String>>
+
+    suspend fun addFavorite(placeId: String)
+
+    suspend fun removeFavorite(placeId: String)
+
+    suspend fun refreshPlaces()
+}
