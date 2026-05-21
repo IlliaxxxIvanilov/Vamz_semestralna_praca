54c518982db621b2f7dfd9249ddf8786912ec480
IlliaxxxIvanilov <illia.ivanilov04@gmail.com>
2026-05-21 14:55:47 +0200

    +add Hilt AppModule and DI setup

---
+++ app/src/main/java/com/example/semestralka/di/AppModule.kt
@@ -0,0 +1,51 @@
+package com.example.semestralka.di
+
+import com.example.semestralka.data.remote.OverpassApiService
+import com.example.semestralka.data.repository.PlacesRepositoryImpl
+import com.example.semestralka.repository.PlacesRepository
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val OVERPASS_BASE_URL = "https://overpass-api.de/"
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi = Moshi.Builder()
+        .addLast(KotlinJsonAdapterFactory())
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(moshi: Moshi): Retrofit = Retrofit.Builder()
+        .baseUrl(OVERPASS_BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideOverpassApiService(retrofit: Retrofit): OverpassApiService =
+        retrofit.create(OverpassApiService::class.java)
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindPlacesRepository(
+        impl: PlacesRepositoryImpl
+    ): PlacesRepository
+}
